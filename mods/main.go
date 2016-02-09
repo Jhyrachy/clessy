8269e40ff348bae76b824b8addc9f916d9fb415c
Hamcha <zikyky@gmail.com>
2016-02-09 10:46:40 +0000

    Better command detection

--- mods/main.go
+++ mods/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"strings"
 
 	"../tg"
 )
@@ -10,8 +11,20 @@ func dispatch(broker *tg.Broker, update tg.APIMessage) {
 	metafora(broker, update)
 }
 
+func isCommand(update tg.APIMessage, cmdname string) bool {
+	if update.Text == nil {
+		return false
+	}
+
+	text := *(update.Text)
+	return strings.HasPrefix(text, "/"+cmdname+"@"+*botname) || (strings.HasPrefix(text, "/"+cmdname) && !strings.Contains(text, "@"))
+}
+
+var botname *string
+
 func main() {
 	brokerAddr := flag.String("broker", "localhost:7314", "Broker address:port")
+	botname = flag.String("botname", "maudbot", "Bot name for /targetet@commands")
 	flag.Parse()
 
 	err := tg.CreateBrokerClient(*brokerAddr, dispatch)
